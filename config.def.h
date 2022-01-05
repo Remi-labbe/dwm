a3b8cb82c05995a83fcff883f8e81e8ddbe4cf44
Remi Labbe <labbe.remi@free.fr>
2022-01-05 15:46:43 +0100

    change term shell to zsh

--- config.def.h
+++ config.def.h
@@ -69,7 +69,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", "run:", NULL };
-static const char *termcmd[]  = { TERMINAL, NULL };
+static const char *termcmd[]  = { TERMINAL, "-e", "zsh", NULL };
 
 #include <X11/XF86keysym.h>
 
